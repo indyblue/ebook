55361c9d42ecc738981a4b7488e2cade372eadfb
DAS <david@indyblue.com>
2017-03-19 14:08:08 -0400

    tweak gitignore for output files tweak quote error reporting to make it easier to find them add code to call calibre to convert html output into epub/mobi

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // requires
 	var fs = require('fs');
 	var path = require('path');
+	var exec = require('child_process').exec;
 	var textile = require('textile.das');
 
 var t = new textile();
+
+/*
+var test = 'p[la]{display:inline-block;}(((<(a s#df). This *is* a _test_, %(r)wonder% if it will work.';
+test = "Lear chef-d'-\oeuvre, Messieurs, c'est de s'\'etre fait nier par ce si\`ecle.";
+console.log(test);
+console.log(t.toHtml(test));
+return;
+// */
 
 var fpath = __dirname;
 if(process.argv.length>2) 
@@ -44,20 +53,54 @@ for(var i=0;i<files.length;i++){
 	var txt = fs.readFileSync(ipath, 'utf8');
 	var txtHtm = t.toHtml(txt);
 	htm += txtHtm;
-	console.log(ipath, txt.length, htm.length);
-	if(/['"]/.test(txtHtm.replace(/<[^>]*>/g,'')))
-		console.log('***** quote error');
+	//console.log(ipath, txt.length, htm.length);
+	txtHtm.replace(/<[^>]*>/g,'').replace(/['"]/g, function(q,i,str){
+		console.log(ipath, txt.length, htm.length);
+		console.log('***** quote error - ' + str.substr(i-50,100));
+	});
 }
 var $doc = t.doc();
 var $txt = $doc.find('#htmlout');
 $txt.html(htm);
 var $fn = $doc.find('#fnout');
 t.fnMove($txt, $fn);
+t.tocBuild('h2','h3');
+
+
+var fnCamel = (str) => str.replace(/([^a-z0-9]*)([a-z0-9]+)([^a-z0-9]*)/gi, function(m,nw1, w, nw2){
+		return w[0].toUpperCase() + w.substr(1).toLowerCase();
+	});
+
+var title1 = $doc.find('.title1').text();
+var title2 = $doc.find('.title2').text();
+var author = $doc.find('.author').text();
+var fname = fnCamel(title1) + '_' + fnCamel(author);
+console.log(title1, title2, author);
+console.log(fname);
 
 var outdir = __dirname; //path.dirname(fpath);
-var outfile = path.join(outdir, 'output.html');
-fs.writeFileSync(outfile, $doc.html());
+var outfile = path.join(outdir, fname);
 
+fs.writeFileSync(outfile+'.html', $doc.html());
 
-//console.log(t.toHtml('p[la]{display:inline-block;}(((<(a s#df). This *is* a _test_, %(r)wonder% if it will work.'));
+var cmd = 'ebook-convert "'+outfile+'.html" "'+outfile+'.{ext}"'
+	+' --title "'+title1+'" '
+	+' --comments "'+title2+'" '
+	+' --authors "'+author+'" '
+	+" --chapter \"//*[name()='h1' or name()='h2']\" "
+	+" --page-breaks-before \"//*[name()='h1' or name()='h2' or name()='h3']\" "
+	+" --level1-toc \"//*[name()='h2']\" "
+	+" --level2-toc \"//*[name()='h3']\" "
+	+" --max-toc-links 500 --duplicate-links-in-toc  --toc-threshold 500";
+
+var type = ['epub', 'mobi'];
+for(var i=0;i<type.length;i++){
+	var cmde = cmd.replace(/{ext}/g,type[i]);
+	console.log(cmde);
+	exec(cmde, function(error, stdout, stderr) {
+	  console.log('stdout', stdout);
+	  console.log('error', error);
+	  console.log('stderr', stderr);
+	});
+}
 
